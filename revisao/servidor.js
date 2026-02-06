4445c334f44361bade154c1594db19ef9b7aec23
DEV <DEV@nsn1191261189.edu119.local>
2026-02-06 16:38:01 -0300

    Projeto Completo

--- revisao/servidor.js
+++ revisao/servidor.js
@@ -7,18 +7,18 @@ const PORTA = 5000
 app.use(express.json())
 
 let listaAlunos = [
-    {id: 1, nome: "Jansen"},
+    {id: 1, nome: "Jansen"}, // 0 = é o índice
     {
-        id: 2, nome: "Vitor"
+        id: 2, nome: "Vitor"  // 1 = é o índice
     },
     {
-        id: 3, nome: "Pedro"
+        id: 3, nome: "Pedro"  // 2 = é o índice
     },
     {
-        id: 4, nome: "Vitor"
+        id: 4, nome: "Vitor"  // 3 = é o índice
     },
     {
-        id: 5, nome: "Higor"
+        id: 5, nome: "Higor"  // 4 = é o índice
     }
     
 ]
@@ -73,7 +73,8 @@ app.get("/alunos/:codigo", (req, res) => {
 
 //Alterar aluno
 app.put("/alunos/:codigo", (req,res) => {
-    const idParametro = Number(req.params.codigo);
+    const idParametro = req.params.codigo ? Number(req.params.codigo):0;
+    console.log("parametro:", req.params)
     const indiceAluno = listaAlunos.findIndex(a => a.id == idParametro) // Localiza o índice
     const { nome } = req.body;
 
@@ -88,8 +89,69 @@ app.put("/alunos/:codigo", (req,res) => {
     }
     listaAlunos[indiceAluno] = {id:idParametro, nome}
     // listaAlunos[indiceAluno].nome = nome;
-    res.status(200).json({msg: "Alteração Feita com sucesso", Indice:indiceAluno})
+    res.status(200).json({msg: "Alteração Feita com sucesso", Indice: indiceAluno})
 
+})
+
+app.put("/alunos/", (req, res) => {
+    const idParametro = req.params.codigo ? Number(req.params.codigo):0
+    console.log("parametros:", req.params)
+    if(idParametro === 0)
+    {
+        return res.status(400).json({msg: "Gentileza Digitar o Nome!"})
+    }
+})
+
+app.delete("/alunos/:codigo", (req, res) => {
+    const idParametro = Number(req.params.codigo) ? Number(req.params.codigo):0
+    const aluno = listaAlunos.findIndex(a => a.id == idParametro)
+    console.log(aluno)
+
+    if (aluno === -1)
+    {
+        return res.status(404).json({msg: "Aluno não encontrado"})
+    }
+
+    const alunoRemovido = listaAlunos.splice(aluno, 1)
+    return res.status(200).json({msg: "Aluno removido com Sucesso!", aluno: alunoRemovido});
+})
+
+app.delete("/alunos/", (req, res) => {
+    const idParametro = req.params.codigo ? Number(req.params.codigo):0
+    console.log("parametros:", req.params)
+    if (idParametro === 0)
+    {
+        return res.status(400).json({msg: "Gentileza Digitar o ID!"})
+    }
+})
+
+
+app.post("/alunos", (req, res) => {
+    const id = req.body.id;
+    const nome = req.body.nome;
+
+    const novoAluno = {id , nome};
+
+    const idEncontrado = listaAlunos.findIndex(a => a.id == Number(id))
+
+    if (!nome)
+    {
+        return res.status(404).json({msg: "Por gentileza colocar o nome"})
+    }
+    if ( id < 0)
+    {
+        return res.status(400).json({msg: "Por favor coloque um id de número positivo"})
+    }
+    if (idEncontrado === -1)
+    {
+        listaAlunos.push(novoAluno);
+        console.log(idEncontrado)
+
+        return res.status(200).json({msg: "Aluno criado com sucesso"})
+    }
+
+    return res.status(400).json({msg: "Aluno já existe"})
+    
 })
 
 app.listen(PORTA, () => { // http://localhost:5000
